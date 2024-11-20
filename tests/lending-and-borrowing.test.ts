60acca81cd52198be9ad011e482842085691b1e2
aosunkojo <aosunkojo@gmail.com>
2024-11-20 12:43:49 +0100

    Add ReadMe

--- tests/lending-and-borrowing.test.ts
+++ tests/lending-and-borrowing.test.ts
@@ -1,5 +1,3 @@
-;; tests/lending-and-borrowing_test.ts
-
 import { Clarinet, Tx, Chain, Account, types } from 'https://deno.land/x/clarinet@v0.14.0/index.ts';
 import { assertEquals } from 'https://deno.land/std@0.90.0/testing/asserts.ts';
 
